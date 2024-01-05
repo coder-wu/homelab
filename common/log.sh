0646213d6519461ec08e923ad7613be22c3a092f
coderwu <coderwumail@gmail.com>
2024-01-05 21:35:23 +0800

    indent with 2 spaces

--- common/log.sh
+++ common/log.sh
@@ -7,30 +7,30 @@ log_file="${log_dir}/$(basename "$0").$(date +"%y%m%d").log"
 readonly log_file
 
 if [ ! -d "${log_dir}" ]; then
-    mkdir -p "${log_dir}" ||
-        echo "Can't create log directory: ${log_dir}, please check the permission." ||
-        exit 1
+  mkdir -p "${log_dir}" ||
+    echo "Can't create log directory: ${log_dir}, please check the permission." ||
+    exit 1
 fi
 
 function log() {
-    printf "$(date +"%D %T") %s \n" "$*" | tee -a "${log_file}"
+  printf "$(date +"%D %T") %s \n" "$*" | tee -a "${log_file}"
 }
 
 function log_info() {
-    log "[INFO] " "$*"
+  log "[INFO] " "$*"
 }
 
 function log_warn() {
-    log "[WARN] " "$*"
+  log "[WARN] " "$*"
 }
 
 function log_error() {
-    log "[ERROR] " "$*"
+  log "[ERROR] " "$*"
 }
 
 # runnig a command or function, record log.
 function log_function() {
-    while read -r line; do
-        log "${line}"
-    done
+  while read -r line; do
+    log "${line}"
+  done
 }
