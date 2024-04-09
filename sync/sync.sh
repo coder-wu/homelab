ddeb8fde7b8d4de9381e4bbcf9d5ad624b957fc7
wu <coderwumail@gmail.com>
2024-04-09 21:56:05 +0800

    use rclone to sync

--- sync/sync.sh
+++ sync/sync.sh
@@ -5,8 +5,17 @@ source "$(dirname "$0")/../common/init.sh"
 
 readonly sync_config="${HOMELAB_HOME}/sync/config"
 
+from_paths=()
+to_paths=()
 while read -r from to || [ -n "${from}" ]; do
+  from_paths+=("${from}")
+  to_paths+=("${to}")
+done <"${sync_config}"
+
+for i in "${!from_paths[@]}"; do 
+  from="${from_paths[$i]}"
+  to="${to_paths[$i]}"
   log_info "sync from ${from} to ${to}"
   rclone sync --interactive --copy-links \
     "${from}" "${to}" 2>&1 | log_function
-done <"${sync_config}"
+done
